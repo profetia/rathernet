68ed22e108b942d982d31d8adfc352b87d9379d5
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-12 22:12:21 +0800

    refactor: rename ASIO into Asio

--- src/raudio/asio.rs
+++ src/raudio/asio.rs
@@ -2,47 +2,47 @@ use anyhow::Result;
 use cpal::{traits::HostTrait, Device, Host};
 use rodio::{OutputStream, OutputStreamHandle, StreamError};
 
-pub struct ASIOHost(Host);
+pub struct AsioHost(Host);
 
-impl ASIOHost {
+impl AsioHost {
     pub fn try_default() -> Result<Self> {
         let host = cpal::host_from_id(cpal::HostId::Asio)?;
         Ok(Self(host))
     }
 }
 
-impl From<ASIOHost> for Host {
-    fn from(value: ASIOHost) -> Self {
+impl From<AsioHost> for Host {
+    fn from(value: AsioHost) -> Self {
         value.0
     }
 }
 
-pub struct ASIOOutputStream(OutputStream);
-pub struct ASIOOutputStreamHandle(OutputStreamHandle);
+pub struct AsioOutputStream(OutputStream);
+pub struct AsioOutputStreamHandle(OutputStreamHandle);
 
-impl ASIOOutputStream {
-    pub fn try_from_device(device: &Device) -> Result<(Self, ASIOOutputStreamHandle)> {
+impl AsioOutputStream {
+    pub fn try_from_device(device: &Device) -> Result<(Self, AsioOutputStreamHandle)> {
         let (stream, handle) = OutputStream::try_from_device(device)?;
-        Ok((ASIOOutputStream(stream), ASIOOutputStreamHandle(handle)))
+        Ok((AsioOutputStream(stream), AsioOutputStreamHandle(handle)))
     }
 
-    pub fn try_default() -> Result<(Self, ASIOOutputStreamHandle)> {
-        let host = ASIOHost::try_default()?;
+    pub fn try_default() -> Result<(Self, AsioOutputStreamHandle)> {
+        let host = AsioHost::try_default()?;
         match host.0.default_output_device() {
-            Some(device) => ASIOOutputStream::try_from_device(&device),
+            Some(device) => AsioOutputStream::try_from_device(&device),
             None => Err(StreamError::NoDevice.into()),
         }
     }
 }
 
-impl From<ASIOOutputStream> for OutputStream {
-    fn from(value: ASIOOutputStream) -> Self {
+impl From<AsioOutputStream> for OutputStream {
+    fn from(value: AsioOutputStream) -> Self {
         value.0
     }
 }
 
-impl From<ASIOOutputStreamHandle> for OutputStreamHandle {
-    fn from(value: ASIOOutputStreamHandle) -> Self {
+impl From<AsioOutputStreamHandle> for OutputStreamHandle {
+    fn from(value: AsioOutputStreamHandle) -> Self {
         value.0
     }
 }
