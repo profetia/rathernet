9bc0f7cef02f2efa177a178741f6c6d7b41a09ef
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-13 14:39:37 +0800

    fix: replace try_from_device with try_from_name
    AsioOutputStream should not be constructible from any device.

--- src/raudio/asio.rs
+++ src/raudio/asio.rs
@@ -1,13 +1,13 @@
 use anyhow::Result;
-use cpal::{traits::HostTrait, Device, Host};
-use rodio::{OutputStream, OutputStreamHandle, StreamError};
+use cpal::{traits::HostTrait, Host};
+use rodio::{DeviceTrait, OutputStream, OutputStreamHandle, StreamError};
 
 pub struct AsioHost {
     pub inner: Host,
 }
 
 impl AsioHost {
-    pub fn try_default() -> Result<Self> {
+    pub fn try_new() -> Result<Self> {
         let host = cpal::host_from_id(cpal::HostId::Asio)?;
         Ok(Self { inner: host })
     }
@@ -19,15 +19,28 @@ pub struct AsioOutputStream {
 }
 
 impl AsioOutputStream {
-    pub fn try_from_device(device: &Device) -> Result<Self> {
-        let (stream, handle) = OutputStream::try_from_device(device)?;
-        Ok(Self { stream, handle })
+    pub fn try_from_name(name: &str) -> Result<Self> {
+        let host = AsioHost::try_new()?;
+        match host
+            .inner
+            .devices()?
+            .find(|d| d.name().map(|s| s == name).unwrap_or(false))
+        {
+            Some(ref device) => {
+                let (stream, handle) = OutputStream::try_from_device(device)?;
+                Ok(Self { stream, handle })
+            }
+            None => Err(StreamError::NoDevice.into()),
+        }
     }
 
     pub fn try_default() -> Result<Self> {
-        let host = AsioHost::try_default()?;
+        let host = AsioHost::try_new()?;
         match host.inner.default_output_device() {
-            Some(device) => AsioOutputStream::try_from_device(&device),
+            Some(ref device) => {
+                let (stream, handle) = OutputStream::try_from_device(device)?;
+                Ok(Self { stream, handle })
+            }
             None => Err(StreamError::NoDevice.into()),
         }
     }
