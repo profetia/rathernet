3c635d7fe2f670d56dfc3a13b8eeae3a8aa5c91c
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-13 12:17:48 +0800

    feat: async audio output stream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::File, io::BufReader};
+
+use anyhow::Result;
+
+use rathernet::raudio::stream::AudioOutputStream;
+use rodio::Decoder;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let stream = AudioOutputStream::try_default()?;
+    let file = BufReader::new(File::open("test.wav")?);
+    let source = Decoder::new(file)?;
+    stream.write(source).await?;
+
+    Ok(())
 }
--- src/raudio/mod.rs
+++ src/raudio/mod.rs
@@ -1,1 +1,2 @@
 pub mod asio;
+pub mod stream;
