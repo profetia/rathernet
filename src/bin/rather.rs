83db2b3971d650641aab35ad1a2f3421639cf663
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-15 17:07:26 +0800

    feat: calibrate on ather

---
+++ src/bin/rather.rs
@@ -0,0 +1,65 @@
+use std::f32::consts::PI;
+
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+use hound::SampleFormat;
+use rathernet::raudio::{AsioDevice, AudioOutputStream, IntoSpec, Track};
+use rodio::DeviceTrait;
+
+#[derive(Debug, Parser)]
+#[clap(name = "rather", version = "0.1.0", author = "Rathernet")]
+#[clap(about = "A command line interface for rathernet ather.", long_about = None)]
+struct RatherCli {
+    #[clap(subcommand)]
+    subcmd: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Calibrate the ather by writing a test signal.
+    Calibrate {
+        /// The elapse time in seconds to write the test signal.
+        #[clap(short, long)]
+        #[arg(default_value = "10")]
+        elapse: u64,
+        /// The device used to write the test signal.
+        #[clap(short, long)]
+        device: Option<String>,
+    },
+}
+
+async fn calibrate(elapse: u64, device: Option<String>) -> Result<()> {
+    let device = match device {
+        Some(name) => AsioDevice::try_from_name(&name)?,
+        None => AsioDevice::try_default()?,
+    };
+    let config = device.inner.default_output_config()?;
+    let stream = AudioOutputStream::try_from_device(&device)?;
+
+    let sample_rate = config.sample_rate().0;
+    let signal_len = (sample_rate as u64 * elapse) as usize;
+    let signal = (0..signal_len)
+        .map(|item| {
+            let t = item as f32 * 2.0 * PI / sample_rate as f32;
+            (t * 1000f32).sin() + (t * 10000f32).sin()
+        })
+        .collect::<Vec<f32>>();
+
+    let mut spec = config.into_spec();
+    spec.sample_format = SampleFormat::Float;
+
+    let track = Track::from_vec(spec, signal);
+
+    stream.write(track.into_iter()).await;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = RatherCli::parse();
+    match cli.subcmd {
+        Commands::Calibrate { elapse, device } => calibrate(elapse, device).await?,
+    }
+    Ok(())
+}
