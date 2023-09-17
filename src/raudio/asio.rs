fd85b6583df64d329d67ae4f338ebb892677a4eb
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-18 00:55:50 +0800

    refactor: replace try_from_name with try_from_device_config

--- src/raudio/asio.rs
+++ src/raudio/asio.rs
@@ -1,27 +1,23 @@
 use anyhow::Result;
-use cpal::{traits::HostTrait, Device, Host};
+use cpal::{traits::HostTrait, Device, Host, SupportedStreamConfig};
 use rodio::{DeviceTrait, OutputStream, OutputStreamHandle, StreamError};
 
-pub struct AsioHost {
-    pub inner: Host,
-}
+pub struct AsioHost(pub Host);
 
 impl AsioHost {
     pub fn try_new() -> Result<Self> {
         let host = cpal::host_from_id(cpal::HostId::Asio)?;
-        Ok(Self { inner: host })
+        Ok(Self(host))
     }
 }
 
-pub struct AsioDevice {
-    pub inner: Device,
-}
+pub struct AsioDevice(pub Device);
 
 impl AsioDevice {
     pub fn try_default() -> Result<Self> {
         let host = AsioHost::try_new()?;
-        match host.inner.default_input_device() {
-            Some(device) => Ok(Self { inner: device }),
+        match host.0.default_input_device() {
+            Some(device) => Ok(Self(device)),
             None => Err(StreamError::NoDevice.into()),
         }
     }
@@ -29,11 +25,11 @@ impl AsioDevice {
     pub fn try_from_name(name: &str) -> Result<Self> {
         let host = AsioHost::try_new()?;
         match host
-            .inner
+            .0
             .devices()?
             .find(|d| d.name().map(|s| s == name).unwrap_or(false))
         {
-            Some(device) => Ok(Self { inner: device }),
+            Some(device) => Ok(Self(device)),
             None => Err(StreamError::NoDevice.into()),
         }
     }
@@ -46,28 +42,21 @@ pub struct AsioOutputStream {
 
 impl AsioOutputStream {
     pub fn try_from_device(device: &AsioDevice) -> Result<Self> {
-        let (stream, handle) = OutputStream::try_from_device(&device.inner)?;
+        let (stream, handle) = OutputStream::try_from_device(&device.0)?;
         Ok(Self { stream, handle })
     }
 
-    pub fn try_from_name(name: &str) -> Result<Self> {
-        let host = AsioHost::try_new()?;
-        match host
-            .inner
-            .devices()?
-            .find(|d| d.name().map(|s| s == name).unwrap_or(false))
-        {
-            Some(ref device) => {
-                let (stream, handle) = OutputStream::try_from_device(device)?;
-                Ok(Self { stream, handle })
-            }
-            None => Err(StreamError::NoDevice.into()),
-        }
+    pub fn try_from_device_config(
+        device: &AsioDevice,
+        config: SupportedStreamConfig,
+    ) -> Result<Self> {
+        let (stream, handle) = OutputStream::try_from_device_config(&device.0, config)?;
+        Ok(Self { stream, handle })
     }
 
     pub fn try_default() -> Result<Self> {
         let host = AsioHost::try_new()?;
-        match host.inner.default_output_device() {
+        match host.0.default_output_device() {
             Some(ref device) => {
                 let (stream, handle) = OutputStream::try_from_device(device)?;
                 Ok(Self { stream, handle })
