d714bafd3bb6d610aa75a6a42895d508789d4d1a
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-18 18:49:57 +0800

    feat: builtin header and symbols

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 use anyhow::Result;
+use cpal::{traits::DeviceTrait, SupportedStreamConfig};
+use rathernet::{rather::AtherStreamConfig, raudio::AsioDevice};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let device = AsioDevice::try_default()?;
+
+    let default_config = device.0.default_output_config()?;
+    let stream_config = SupportedStreamConfig::new(
+        1,
+        cpal::SampleRate(48000),
+        default_config.buffer_size().clone(),
+        default_config.sample_format(),
+    );
+
+    let config = AtherStreamConfig::new(stream_config);
+
+    println!("{:?}", config.header);
+
     Ok(())
 }
---
+++ src/rather/stream.rs
@@ -0,0 +1,67 @@
+use std::f32::consts::PI;
+
+use cpal::SupportedStreamConfig;
+
+use crate::raudio::AudioOutputStream;
+
+use super::{Frame, Header, Symbol};
+
+pub struct AtherStreamConfig {
+    pub frequency: u32,
+    pub bit_rate: u32,
+    pub header: Header,
+    pub symbols: (Symbol, Symbol),
+    pub stream_config: SupportedStreamConfig,
+}
+
+impl AtherStreamConfig {
+    pub fn new(stream_config: SupportedStreamConfig) -> Self {
+        let frequency = 10000u32;
+        let bit_rate = 1000u32;
+        let header_len = 480u32;
+
+        let elapse = 1.0 / bit_rate as f32;
+        let sample_rate = stream_config.sample_rate().0;
+        let symbol_zero = (0..(elapse * sample_rate as f32) as usize)
+            .map(|item| {
+                let t = item as f32 * 2.0 * PI / sample_rate as f32;
+                (t * frequency as f32).sin()
+            })
+            .collect::<Symbol>();
+        let symbol_one = symbol_zero.iter().map(|item| -item).collect::<Symbol>();
+
+        let header = (0..header_len)
+            .map(|item| {
+                if item < header_len / 2 {
+                    2000.0 + item as f32 * 6000.0 / (header_len / 2) as f32
+                } else {
+                    8000.0 - (item - header_len / 2) as f32 * 6000.0 / (header_len / 2) as f32
+                }
+            })
+            .scan(0.0f32, |acc, item| {
+                *acc += item / sample_rate as f32;
+                Some(*acc)
+            })
+            .map(|item| (item * 2.0 * PI).sin())
+            .collect::<Header>();
+
+        Self {
+            frequency,
+            bit_rate,
+            header,
+            symbols: (symbol_zero, symbol_one),
+            stream_config,
+        }
+    }
+}
+
+pub struct AtherOutputStream {
+    config: AtherStreamConfig,
+    stream: AudioOutputStream<Frame>,
+}
+
+impl AtherOutputStream {
+    pub fn new(config: AtherStreamConfig, stream: AudioOutputStream<Frame>) -> Self {
+        Self { config, stream }
+    }
+}
