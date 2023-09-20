f337b9bc073252ed3502b132b3e03d961c92645d
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-20 21:45:07 +0800

    feat: trait ContinuousStream that only lazy on first poll

--- src/raudio/mod.rs
+++ src/raudio/mod.rs
@@ -3,5 +3,5 @@ mod stream;
 mod track;
 
 pub use asio::{AsioDevice, AsioHost, AsioOutputStream};
-pub use stream::{AudioInputStream, AudioOutputFuture, AudioOutputStream};
+pub use stream::{AudioInputStream, AudioOutputFuture, AudioOutputStream, ContinuousStream};
 pub use track::{AudioSamples, AudioTrack, SharedSamples, SharedTrack};
