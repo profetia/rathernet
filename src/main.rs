1afa1670fed018a90299f828017919943dbd04ca
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-22 01:20:04 +0800

    feat: frame-based read on the ather

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use anyhow::Result;
+use bitvec::prelude::*;
+use cpal::FromSample;
 use cpal::{traits::DeviceTrait, SupportedStreamConfig};
+use rathernet::rather::{self, AtherInputStream};
 use rathernet::{
     rather::{AtherOutputStream, AtherStreamConfig},
-    raudio::{AsioDevice, AudioOutputStream},
+    raudio::{AsioDevice, AudioInputStream, AudioOutputStream},
 };
+use rodio::Decoder;
+use std::io::Write;
+use std::sync::Arc;
+use std::{
+    fs::{self, File},
+    io::BufReader,
+};
+use tokio::sync;
+use tokio_stream::StreamExt;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -17,7 +29,50 @@ async fn main() -> Result<()> {
         default_config.sample_format(),
     );
 
+    let write_stream = AudioOutputStream::try_from_device_config(&device, config.clone())?;
+    let read_stream = AudioInputStream::try_from_device_config(&device, config.clone())?;
+
     let config = AtherStreamConfig::new(10000, 1000, config);
+    let write_ather = AtherOutputStream::new(config.clone(), write_stream);
+    let mut read_ather = AtherInputStream::new(config, read_stream);
+
+    // let file = fs::read_to_string("input.txt")?;
+    let file = fs::read_to_string("assets/ather/INPUT.txt")?;
+    let mut bits = bitvec![];
+    for ch in file.chars() {
+        match ch {
+            '0' => bits.push(false),
+            '1' => bits.push(true),
+            _ => {}
+        }
+    }
+
+    tokio::join!(write_ather.write(&bits), async {
+        let mut received = 0;
+        while let Some(bits) = read_ather.next().await {
+            // if let Some(bits) = read_ather.next().await {
+            println!("{}", bits);
+            received += bits.len();
+            eprintln!("Received: {}", received);
+        }
+    });
+
+    // let mut buf = Decoder::new(BufReader::new(File::open("test.wav")?))?
+    //     .map(f32::from_sample_)
+    //     .collect::<Vec<f32>>();
+
+    // let read_stream = Arc::new(sync::Mutex::new(read_stream));
+    // let bits = rather::decode_packet(&config, &read_stream, &mut buf)
+    //     .await
+    //     .unwrap();
+
+    // // Create a new file and write the bits to it as a string
+    // let mut file = File::create("output.txt")?;
+    // let content = bits
+    //     .iter()
+    //     .map(|bit| if *bit { '1' } else { '0' })
+    //     .collect::<String>();
+    // file.write_all(content.as_bytes())?;
 
     Ok(())
 }
--- src/rather/stream.rs
+++ src/rather/stream.rs
@@ -6,17 +6,42 @@
 //! start of a stream. The length field is used to indicate the length of the body. The checksum
 //! field is used to verify the integrity of the stream. The body is the actual data to be sent.
 
-// TODO: implement length field and checksum field
+// TODO: implement the parity of length field and checksum field
 
-use super::{frame::Header, Body, Frame, Preamble, Symbol};
-use crate::raudio::AudioOutputStream;
+use super::{
+    frame::Header,
+    signal::{self, BandPass},
+    Body, Frame, Preamble, Symbol, Warmup,
+};
+use crate::raudio::{
+    AudioInputStream, AudioOutputStream, AudioSamples, AudioTrack, ContinuousStream,
+};
 use bitvec::prelude::*;
 use cpal::SupportedStreamConfig;
-use std::time::Duration;
+use std::{
+    mem,
+    pin::Pin,
+    sync::{Arc, Mutex},
+    task::{self, Poll, Waker},
+    time::Duration,
+};
+use tokio::sync::{
+    self,
+    mpsc::{self, UnboundedSender},
+};
+use tokio_stream::{Stream, StreamExt};
 
+const WARMUP_LEN: usize = 8;
+const PREAMBLE_LEN: usize = 8;
+const LENGTH_LEN: usize = 7;
+const PAYLOAD_LEN: usize = 127;
+const CORR_THRESHOLD: f32 = 0.15;
+
+#[derive(Debug, Clone)]
 pub struct AtherStreamConfig {
     pub frequency: u32,
     pub bit_rate: u32,
+    pub warmup: Warmup,
     pub preamble: Preamble,
     pub symbols: (Symbol, Symbol),
     pub stream_config: SupportedStreamConfig,
@@ -30,7 +55,8 @@ impl AtherStreamConfig {
         Self {
             frequency,
             bit_rate,
-            preamble: Preamble::new(sample_rate, duration),
+            warmup: Warmup::new(WARMUP_LEN, sample_rate, duration),
+            preamble: Preamble::new(PREAMBLE_LEN, sample_rate, duration),
             symbols: Symbol::new(frequency, sample_rate, duration),
             stream_config,
         }
@@ -39,60 +65,89 @@ impl AtherStreamConfig {
 
 pub struct AtherOutputStream {
     config: AtherStreamConfig,
-    stream: AudioOutputStream<Frame>,
+    stream: AudioOutputStream<AudioTrack<f32>>,
 }
 
 impl AtherOutputStream {
-    pub fn new(config: AtherStreamConfig, stream: AudioOutputStream<Frame>) -> Self {
+    pub fn new(config: AtherStreamConfig, stream: AudioOutputStream<AudioTrack<f32>>) -> Self {
         Self { config, stream }
     }
 }
 
 impl AtherOutputStream {
-    fn encode(&self, bits: &BitSlice) -> Vec<Frame> {
-        let mut frames = vec![];
-        for chunk in bits.chunks(127) {
-            let payload = chunk.encode(self.config.symbols.clone());
-            let length = (chunk.len() as u8).encode(self.config.symbols.clone())[..7].to_owned();
-
-            frames.push(Frame::new(
-                self.config.stream_config.clone(),
-                Header::new(self.config.preamble.clone(), length),
-                Body::new(payload),
-            ));
-        }
-        if bits.len() % 127 == 0 {
-            let payload = vec![];
-            let length = 0u8.encode(self.config.symbols.clone())[..7].to_owned();
-
-            frames.push(Frame::new(
-                self.config.stream_config.clone(),
-                Header::new(self.config.preamble.clone(), length),
-                Body::new(payload),
-            ));
-        }
-
-        frames
-    }
-
     pub async fn write(&self, bits: &BitSlice) {
-        let frames = self.encode(bits);
-        for frame in frames {
-            self.stream.write(frame).await;
-        }
+        let mut frames = vec![create_warmup(&self.config)];
+        frames.extend(encode_packet(&self.config, bits));
+
+        let track = AudioTrack::new(
+            self.config.stream_config.clone(),
+            frames
+                .into_iter()
+                .map(|frame| frame.into())
+                .collect::<Vec<AudioSamples<f32>>>()
+                .concat()
+                .into(),
+        );
+        self.stream.write(track).await;
     }
 
     pub async fn write_timeout(&self, bits: &BitSlice, timeout: Duration) {
-        let frames = self.encode(bits);
+        let mut frames = vec![create_warmup(&self.config)];
+        frames.extend(encode_packet(&self.config, bits));
+
+        let track = AudioTrack::new(
+            self.config.stream_config.clone(),
+            frames
+                .into_iter()
+                .map(|frame| frame.into())
+                .collect::<Vec<AudioSamples<f32>>>()
+                .concat()
+                .into(),
+        );
         tokio::select! {
             _ = async {
-                for frame in frames {
-                    self.stream.write(frame).await;
-                }
+                self.stream.write(track).await;
             } => {}
             _ = tokio::time::sleep(timeout) => {}
         };
     }
+}
+
+fn create_warmup(config: &AtherStreamConfig) -> Frame {
+    Frame::new(
+        config.stream_config.clone(),
+        Header::new(
+            config.warmup.clone().into(),
+            0u8.encode(config.symbols.clone()),
+        ),
+        Body::new(vec![]),
+    )
+}
+
+fn encode_packet(config: &AtherStreamConfig, bits: &BitSlice) -> Vec<Frame> {
+    let mut frames = vec![];
+    for chunk in bits.chunks(PAYLOAD_LEN) {
+        let payload = chunk.encode(config.symbols.clone());
+        let length = (chunk.len() as u8).encode(config.symbols.clone())[..LENGTH_LEN].to_owned();
+
+        frames.push(Frame::new(
+            config.stream_config.clone(),
+            Header::new(config.preamble.clone(), length),
+            Body::new(payload),
+        ));
+    }
+    if bits.len() % PAYLOAD_LEN == 0 {
+        let payload = vec![];
+        let length = 0u8.encode(config.symbols.clone())[..LENGTH_LEN].to_owned();
+
+        frames.push(Frame::new(
+            config.stream_config.clone(),
+            Header::new(config.preamble.clone(), length),
+            Body::new(payload),
+        ));
+    }
+
+    frames
 }
 
 trait AtherEncoding {
@@ -125,5 +180,254 @@ impl AtherEncoding for BitSlice {
             }
         }
         samples
+    }
+}
+
+pub struct AtherInputStream {
+    task: AtherInputTask,
+    sender: UnboundedSender<()>,
+    stream: Arc<sync::Mutex<AudioInputStream<f32>>>,
+}
+
+impl AtherInputStream {
+    pub fn new(config: AtherStreamConfig, stream: AudioInputStream<f32>) -> Self {
+        let (sender, mut reciever) = mpsc::unbounded_channel();
+        let task = Arc::new(Mutex::new(AtherInputTaskState::Pending));
+        let stream = Arc::new(sync::Mutex::new(stream));
+        tokio::spawn({
+            let task = task.clone();
+            let stream = stream.clone();
+            async move {
+                let mut buf = vec![];
+                while (reciever.recv().await).is_some() {
+                    match decode_packet(&config, &stream, &mut buf).await {
+                        Some(bits) => {
+                            let mut guard = task.lock().unwrap();
+                            match guard.take() {
+                                AtherInputTaskState::Running(waker) => {
+                                    *guard = AtherInputTaskState::Completed(bits);
+                                    waker.wake();
+                                }
+                                content => *guard = content,
+                            }
+                        }
+                        None => {
+                            buf.clear();
+                        }
+                    }
+                }
+            }
+        });
+        Self {
+            sender,
+            task,
+            stream,
+        }
+    }
+}
+
+type AtherInputTask = Arc<Mutex<AtherInputTaskState>>;
+
+enum AtherInputTaskState {
+    Pending,
+    Running(Waker),
+    Completed(BitVec),
+    Suspended(Option<BitVec>),
+}
+
+impl AtherInputTaskState {
+    fn take(&mut self) -> AtherInputTaskState {
+        mem::replace(self, AtherInputTaskState::Suspended(None))
+    }
+}
+
+impl Stream for AtherInputStream {
+    type Item = BitVec;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> Poll<Option<Self::Item>> {
+        let mut guard = self.task.lock().unwrap();
+        match guard.take() {
+            AtherInputTaskState::Pending => {
+                *guard = AtherInputTaskState::Running(cx.waker().clone());
+                self.sender.send(()).unwrap();
+                Poll::Pending
+            }
+            AtherInputTaskState::Running(_) => {
+                *guard = AtherInputTaskState::Running(cx.waker().clone());
+                Poll::Pending
+            }
+            AtherInputTaskState::Completed(bits) => {
+                *guard = AtherInputTaskState::Pending;
+                Poll::Ready(Some(bits))
+            }
+            AtherInputTaskState::Suspended(bits) => {
+                if let Some(bits) = bits {
+                    *guard = AtherInputTaskState::Suspended(None);
+                    Poll::Ready(Some(bits))
+                } else {
+                    Poll::Ready(None)
+                }
+            }
+        }
+    }
+}
+
+async fn receive_sample(
+    stream: &Arc<sync::Mutex<AudioInputStream<f32>>>,
+) -> Option<AudioSamples<f32>> {
+    let mut guard = stream.lock().await;
+    guard.next().await
+}
+
+async fn decode_packet(
+    // async fn decode_frame(
+    config: &AtherStreamConfig,
+    stream: &Arc<sync::Mutex<AudioInputStream<f32>>>,
+    buf: &mut Vec<f32>,
+) -> Option<BitVec> {
+    let sample_rate = config.stream_config.sample_rate().0 as f32;
+    let band_pass = (
+        config.frequency as f32 - 1000.,
+        config.frequency as f32 + 1000.,
+    );
+    let preamble_len = config.preamble.0.len();
+    let symbol_len = config.symbols.0 .0.len();
+
+    println!("Start decode");
+
+    loop {
+        println!(
+            "Looping on the preamble {}, expect {}",
+            buf.len(),
+            preamble_len
+        );
+        if buf.len() >= preamble_len {
+            let (index, value) = signal::synchronize(&config.preamble.0, buf);
+            if index >= 0 {
+                let index = index as usize;
+                println!("Got index {} with {}", index, value);
+                if value > CORR_THRESHOLD && index + preamble_len < buf.len() {
+                    *buf = buf.split_off(index + preamble_len);
+                    break;
+                }
+                println!(
+                    "Failed to comform the threshold, got {}, len {}",
+                    value,
+                    buf.len()
+                );
+            }
+            println!("Failed to find a start, len {}", buf.len());
+        }
+
+        println!("Wait for more data");
+        match receive_sample(stream).await {
+            Some(sample) => buf.extend(sample.iter()),
+            None => return None,
+        }
+        println!("Done");
+    }
+
+    println!("Preamble found");
+
+    let (mut length, mut index) = (0u8, 0usize);
+    while index < LENGTH_LEN {
+        if buf.len() > symbol_len {
+            buf.band_pass(sample_rate, band_pass);
+            let value = signal::dot_product(&config.symbols.0 .0, buf[..symbol_len].as_ref());
+            println!("length value {}", value);
+            if value <= 0. {
+                length += 1 << index;
+            }
+
+            *buf = buf.split_off(symbol_len);
+            index += 1;
+        } else {
+            match receive_sample(stream).await {
+                Some(sample) => buf.extend(sample.iter()),
+                None => return None,
+            }
+        }
+    }
+
+    println!("Found length {}", length);
+
+    let (mut bits, mut index) = (bitvec![], 0u8);
+    while index < length {
+        if buf.len() > symbol_len {
+            buf.band_pass(sample_rate, band_pass);
+            let value = signal::dot_product(&config.symbols.0 .0, buf[..symbol_len].as_ref());
+            if value > 0. {
+                bits.push(false);
+            } else {
+                bits.push(true);
+            }
+
+            *buf = buf.split_off(symbol_len);
+            index += 1;
+        } else {
+            match receive_sample(stream).await {
+                Some(sample) => buf.extend(sample.iter()),
+                None => return None,
+            }
+        }
+    }
+
+    Some(bits)
+}
+
+// async fn decode_packet(
+//     config: &AtherStreamConfig,
+//     stream: &Arc<sync::Mutex<AudioInputStream<f32>>>,
+//     buf: &mut Vec<f32>,
+// ) -> Option<BitVec> {
+//     let mut bits = bitvec![];
+//     loop {
+//         match decode_frame(config, stream, buf).await {
+//             Some(frame) => {
+//                 if frame.is_empty() {
+//                     break;
+//                 } else {
+//                     bits.extend(frame);
+//                 }
+//             }
+//             None => return None,
+//         }
+//     }
+//     Some(bits)
+// }
+
+impl ContinuousStream for AtherInputStream {
+    fn resume(&self) {
+        let mut guard = self.task.lock().unwrap();
+        match guard.take() {
+            AtherInputTaskState::Suspended(bits) => {
+                if let Some(bits) = bits {
+                    *guard = AtherInputTaskState::Completed(bits);
+                } else {
+                    *guard = AtherInputTaskState::Pending;
+                }
+                let guard = self.stream.blocking_lock();
+                guard.resume();
+            }
+            content => *guard = content,
+        }
+    }
+
+    fn suspend(&self) {
+        let mut guard = self.task.lock().unwrap();
+        match guard.take() {
+            AtherInputTaskState::Pending => {
+                *guard = AtherInputTaskState::Suspended(None);
+            }
+            AtherInputTaskState::Running(_) => {
+                *guard = AtherInputTaskState::Suspended(None);
+                let guard = self.stream.blocking_lock();
+                guard.suspend();
+            }
+            AtherInputTaskState::Completed(bits) => {
+                *guard = AtherInputTaskState::Suspended(Some(bits));
+            }
+            content => *guard = content,
+        }
     }
 }
