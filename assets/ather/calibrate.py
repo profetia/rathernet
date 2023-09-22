54ffbe13979578749e24cf5aee01e155fad378d6
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-22 12:34:46 +0800

    chore: remove hardcoded length in calibrate.py

--- assets/ather/calibrate.py
+++ assets/ather/calibrate.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee74918847176bced5d3e6d958794742ed80322f3613824c8f06589ecaef7576
-size 1299
+oid sha256:169169ff2548efecac9b7bfe459df61b2c191dd93b860abfe1c60ea04098265e
+size 1242
