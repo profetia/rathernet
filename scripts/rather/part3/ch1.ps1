4e3088950d7770958d26e057ee28288e391091f6
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-25 20:23:14 +0800

    chore: change the script to run in release build

--- scripts/rather/part3/ch1.ps1
+++ scripts/rather/part3/ch1.ps1
@@ -1,5 +1,9 @@
 git checkout -b part3 9a43ff4
 
-cargo run --bin rather -- duplex -c ./assets/ather/INPUT.txt -f ./output.txt
+cargo build --release
+
+./target/release/rather duplex -c ./assets/ather/INPUT.txt -f ./output.txt
 
 git checkout main
+
+git branch -D part3
