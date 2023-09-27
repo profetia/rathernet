07a7e9646866f53b68ed2372d39c8fa5009842ec
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-27 20:00:35 +0800

    revert: use unencoded stream for acsma
    Refs: 9a43ff4.

--- src/rather/stream.rs
+++ src/rather/stream.rs
@@ -1,39 +1,20 @@
 //! # Rather Streams
-//! Rather streams are used to send and receive data on an ather. The data is encoded in the form
-//! of audio signals in the method of phase shift keying (PSK). A frame in the stream consists as
-//! follows:
-//! - Preamble (PREAMBLE_SYMBOL_LEN symbols): A sequence of symbols to indicate the start of a
-//!  frame.
-//! - Header (HEADER_BITS_LEN bits): The header of the frame, containing the encoded result of the
-//!  meta data and the parity.
-//!     - Meta (META_BITS_LEN bits): A sequence of bits to indicate the metadata of the frame, encoded
-//!      with convolutional code.
-//!         - Flag (META_FLAG_BITS_LEN bits): A bit to indicate if the frame is the last frame of the
-//!          packet. 1 for the last frame, 0 for the rest.
-//!         - Type (META_TYPE_BITS_LEN bits): A bit to indicate the type of the frame. 1 for data, 0
-//!          for parity.    
-//!         - Reserved (META_RESERVED_BITS_LEN bits): Reserved for future use.
-//!     - Parity (PARITY_BITS_LEN bits): The parity of the body.
-//! - Body (BODY_BITS_LEN bits): The body of the frame, containing the encoded result of the
-//!  length and payload.
-//!     - Length (LENGTH_BITS_LEN bits): The length of the payload.
-//!     - Payload (PAYLOAD_BITS_LEN bits): The payload of the frame.
+//! Rather streams are used to send and receive data on an ather. The data is encoded in the form of
+//! audio signals in the method of phase shift keying (PSK). The stream is composed of a preamble
+//! (PREAMBLE_SYMBOL_LEN symbols), a length (LENGTH_BITS_LEN symbols) and a payload (PAYLOAD_BITS_LEN
+//! symbols with maximum 1 << LENGTH_BITS_LEN - 1 symbols). The preamble is used to identify the
+//! start of a frame. The length is used to indicate the length of the payload.
 
-extern crate reed_solomon_erasure;
 use super::{
-    conv::ConvCode,
-    encode::{DecodeToInt, EncodeFromBytes},
+    encode::DecodeToInt,
     signal::{self, BandPass},
     Preamble, Symbol, Warmup,
 };
-use crate::{
-    rather::encode::DecodeToBytes,
-    raudio::{AudioInputStream, AudioOutputStream, AudioSamples, AudioTrack, ContinuousStream},
+use crate::raudio::{
+    AudioInputStream, AudioOutputStream, AudioSamples, AudioTrack, ContinuousStream,
 };
 use bitvec::prelude::*;
 use cpal::SupportedStreamConfig;
-use crc::{Crc, CRC_16_IBM_SDLC};
-use reed_solomon_erasure::galois_8::ReedSolomon;
 use std::{
     mem,
     pin::Pin,
@@ -48,40 +29,8 @@ const WARMUP_SYMBOL_LEN: usize = 8;
 const PREAMBLE_SYMBOL_LEN: usize = 32; // 8 | 16 | 32 | 64
 const PREAMBLE_CORR_THRESHOLD: f32 = 0.15;
 
-const CONV_GENERATORS: &[usize] = &[5, 7];
-const CONV_FACTOR: usize = CONV_GENERATORS.len();
-const CONV_ORDER: usize = {
-    let mut max = 0usize;
-    let mut index = 0;
-    while index < CONV_GENERATORS.len() {
-        if max < CONV_GENERATORS[index] {
-            max = CONV_GENERATORS[index];
-        }
-        index += 1;
-    }
-    max.ilog2() as usize
-};
-
-const META_FLAG_BITS_LEN: usize = 1;
-const META_TYPE_BITS_LEN: usize = 1;
-const META_RESERVED_BITS_LEN: usize = 2;
-const META_BITS_LEN: usize = META_FLAG_BITS_LEN + META_FLAG_BITS_LEN + META_RESERVED_BITS_LEN;
-
-const PARITY_ALGORITHM: Crc<u16> = Crc::<u16>::new(&CRC_16_IBM_SDLC);
-const PARITY_RATIO: f32 = 0.25;
-const PARITY_BYTE_LEN: usize = 2;
-const PARITY_BITS_LEN: usize = PARITY_BYTE_LEN * 8;
-
-const LENGTH_BYTE_LEN: usize = 2;
-const LENGTH_BITS_LEN: usize = LENGTH_BYTE_LEN * 8;
-const PAYLOAD_BYTE_LEN: usize = 8;
-const PAYLOAD_BITS_LEN: usize = PAYLOAD_BYTE_LEN * 8;
-
-const META_ENCODED_BITS_LEN: usize = (META_BITS_LEN + CONV_ORDER) * CONV_FACTOR;
-const HEADER_BITS_LEN: usize = META_ENCODED_BITS_LEN + PARITY_BITS_LEN;
-const BODY_BITS_LEN: usize = LENGTH_BITS_LEN + PAYLOAD_BITS_LEN;
-#[allow(dead_code)]
-const PACKET_BITS_LEN: usize = HEADER_BITS_LEN + BODY_BITS_LEN;
+const LENGTH_BITS_LEN: usize = 7; // 5 | 6
+const PAYLOAD_BITS_LEN: usize = (1 << LENGTH_BITS_LEN) - 1;
 
 #[derive(Debug, Clone)]
 pub struct AtherStreamConfig {
@@ -142,65 +91,37 @@ impl AtherOutputStream {
 }
 
 fn encode_packet(config: &AtherStreamConfig, bits: &BitSlice) -> Vec<AudioSamples<f32>> {
-    let mut shards = bits
-        .chunks(PAYLOAD_BITS_LEN)
-        .map(|chunk| {
-            let length = DecodeToBytes::decode(&chunk.len().view_bits()[..LENGTH_BITS_LEN]);
-            let payload = DecodeToBytes::decode(chunk);
+    let mut frames = vec![];
+    for chunk in bits.chunks(PAYLOAD_BITS_LEN) {
+        let payload = chunk.encode(&config.symbols);
+        let length = chunk.len().encode(&config.symbols)[..LENGTH_BITS_LEN].to_owned();
 
-            let mut shard = vec![];
-            shard.extend(length);
-            shard.extend(payload);
-            shard.resize(LENGTH_BYTE_LEN + PAYLOAD_BYTE_LEN, 0u8);
+        frames.push(
+            [
+                config.preamble.0.clone(),
+                length.into_iter().collect(),
+                payload.into_iter().collect(),
+            ]
+            .concat()
+            .into(),
+        );
+    }
+    if bits.len() % PAYLOAD_BITS_LEN == 0 {
+        let payload: Vec<Symbol> = vec![];
+        let length = 0usize.encode(&config.symbols)[..LENGTH_BITS_LEN].to_owned();
 
-            shard
-        })
-        .collect::<Vec<_>>();
-    let data_shards = shards.len();
-    let parity_shards = (data_shards as f32 * PARITY_RATIO) as usize;
-    shards.resize(
-        data_shards + parity_shards,
-        vec![0u8; LENGTH_BYTE_LEN + PAYLOAD_BYTE_LEN],
-    );
-    // eprintln!(
-    //     "Generate {} data shards and {} parity shards",
-    //     data_shards, parity_shards
-    // );
+        frames.push(
+            [
+                config.preamble.0.clone(),
+                length.into_iter().collect(),
+                payload.into_iter().collect(),
+            ]
+            .concat()
+            .into(),
+        );
+    }
 
-    let reed = ReedSolomon::new(data_shards, parity_shards).unwrap();
-    reed.encode(&mut shards).unwrap();
-
-    let conv = ConvCode::new(CONV_GENERATORS);
-    shards
-        .into_iter()
-        .enumerate()
-        .map(|(index, shard)| {
-            let mut meta = bitvec![];
-            meta.push(index == data_shards + parity_shards - 1);
-            meta.push(index < data_shards);
-            meta.extend(bitvec![0; META_RESERVED_BITS_LEN]);
-            meta = conv.encode(&meta);
-
-            let parity = PARITY_ALGORITHM.checksum(&shard) as usize;
-            let parity = &parity.view_bits::<Lsb0>()[..PARITY_BITS_LEN];
-
-            let mut header = bitvec![];
-            header.extend(meta);
-            header.extend(parity);
-
-            let body = shard.encode();
-
-            let mut frame = vec![];
-            frame.extend(header.encode(&config.symbols));
-            frame.extend(body.encode(&config.symbols));
-
-            let mut samples = vec![];
-            samples.push(config.preamble.0.clone());
-            samples.extend(frame.into_iter().map(|symbol| symbol.0).collect::<Vec<_>>());
-
-            samples.concat().into()
-        })
-        .collect::<Vec<_>>()
+    frames
 }
 
 trait AtherSymbolEncoding {
@@ -383,7 +304,7 @@ async fn decode_frame(
     config: &AtherStreamConfig,
     stream: &mut AudioInputStream<f32>,
     buf: &mut Vec<f32>,
-) -> Option<(BitVec, usize, Vec<u8>)> {
+) -> Option<BitVec> {
     let sample_rate = config.stream_config.sample_rate().0 as f32;
     let band_pass = (
         config.frequency as f32 - 1000.,
@@ -391,7 +312,6 @@ async fn decode_frame(
     );
     let preamble_len = config.preamble.0.len();
     let symbol_len = config.symbols.0 .0.len();
-    let conv = ConvCode::new(CONV_GENERATORS);
 
     // println!("Decode frame...");
 
@@ -413,13 +333,13 @@ async fn decode_frame(
         }
     }
 
-    let mut header = bitvec![];
-    while header.len() < HEADER_BITS_LEN {
+    let mut length = bitvec![];
+    while length.len() < LENGTH_BITS_LEN {
         if buf.len() >= symbol_len {
             let mut symbol = buf[..symbol_len].to_owned();
             symbol.band_pass(sample_rate, band_pass);
             let value = signal::dot_product(&config.symbols.1 .0, &symbol);
-            header.push(value > 0.);
+            length.push(value > 0.);
             *buf = buf.split_off(symbol_len);
         } else {
             match stream.next().await {
@@ -429,13 +349,15 @@ async fn decode_frame(
         }
     }
 
-    let mut body = bitvec![];
-    while body.len() < BODY_BITS_LEN {
+    let length = length.decode();
+
+    let mut payload = bitvec![];
+    while payload.len() < length {
         if buf.len() >= symbol_len {
             let mut symbol = buf[..symbol_len].to_owned();
             symbol.band_pass(sample_rate, band_pass);
             let value = signal::dot_product(&config.symbols.1 .0, &symbol);
-            body.push(value > 0.);
+            payload.push(value > 0.);
 
             *buf = buf.split_off(symbol_len);
         } else {
@@ -446,18 +368,7 @@ async fn decode_frame(
         }
     }
 
-    let meta = &header[..META_ENCODED_BITS_LEN];
-    let (meta, _err) = conv.decode(meta);
-    // println!("Receive meta: {}", meta);
-
-    let parity = &header[META_ENCODED_BITS_LEN..];
-    let parity = DecodeToInt::<usize>::decode(parity);
-    // println!("Receive parity: {}", parity);
-
-    let body = DecodeToBytes::decode(&body);
-    // println!("Receive body: {:?}", body);
-
-    Some((meta, parity, body))
+    Some(payload)
 }
 
 async fn decode_packet(
@@ -465,59 +376,20 @@ async fn decode_packet(
     stream: &mut AudioInputStream<f32>,
     buf: &mut Vec<f32>,
 ) -> Option<BitVec> {
-    let mut shards = vec![];
-    let (mut data_shards, mut parity_shards) = (0, 0);
+    let mut bits = bitvec![];
     loop {
         match decode_frame(config, stream, buf).await {
-            Some((meta, parity, body)) => {
-                let recieved = PARITY_ALGORITHM.checksum(&body) as usize;
-                if recieved == parity {
-                    shards.push(Some(body));
-                } else {
-                    shards.push(None);
-                }
-
-                let r#type = meta[META_FLAG_BITS_LEN + META_TYPE_BITS_LEN - 1];
-                if r#type {
-                    data_shards += 1;
-                } else {
-                    parity_shards += 1;
-                }
-
-                let flag = meta[META_FLAG_BITS_LEN - 1];
-                eprintln!(
-                    "Receive frame {} (flag {}, type {}, ok {})",
-                    shards.len(),
-                    flag,
-                    r#type,
-                    recieved == parity
-                );
-                if flag {
+            Some(frame) => {
+                let len = frame.len();
+                bits.extend(frame);
+                eprintln!("Recieved {}, New {}", bits.len(), len);
+                if len != PAYLOAD_BITS_LEN {
                     break;
                 }
             }
             None => return None,
         }
     }
-
-    eprintln!(
-        "Reconstruct shards from {} data shards and {} parity shards",
-        data_shards, parity_shards
-    );
-    let reed = ReedSolomon::new(data_shards, parity_shards).unwrap();
-    reed.reconstruct(&mut shards).unwrap();
-
-    let mut bits = bitvec![];
-    for (index, shard) in shards.into_iter().flatten().take(data_shards).enumerate() {
-        let mut length = DecodeToInt::<usize>::decode(&shard[..LENGTH_BYTE_LEN].encode());
-        if length > PAYLOAD_BITS_LEN {
-            length = PAYLOAD_BITS_LEN;
-        }
-        eprintln!("Decode frame {} (length {})", index, length);
-        let payload = &shard[LENGTH_BYTE_LEN..];
-        bits.extend(&payload.encode()[..length]);
-    }
-
     Some(bits)
 }
 
