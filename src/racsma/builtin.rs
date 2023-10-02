49599cf278c024f569575e8b6943d448807e943a
yanglinshu <nicolasvandarkholme@outlook.com>
2023-10-02 12:38:26 +0800

    feat: SIFS and DIFS timeout

--- src/racsma/builtin.rs
+++ src/racsma/builtin.rs
@@ -30,5 +30,7 @@ pub const PAYLOAD_BITS_LEN: usize = ATHER_PAYLOAD_BITS_LEN
     - SEQ_BITS_LEN
     - PARITY_BITS_LEN;
 
-pub const ACK_LINK_ERROR_THRESHOLD: usize = 5;
-pub const ACK_RECIEVE_TIMEOUT: Duration = Duration::from_millis(3000);
+pub const ACK_LINK_ERROR_THRESHOLD: usize = 10;
+pub const ACK_RECIEVE_TIMEOUT: Duration = Duration::from_millis(200);
+pub const ACK_SIFS_TIMEOUT: Duration = Duration::from_millis(20);
+pub const ACK_DIFS_TIMEOUT: Duration = Duration::from_millis(50);
