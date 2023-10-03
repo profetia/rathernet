8d20848ab1c61d2e545e6d660e389919ee5d74bc
winlere <winlere@qq.com>
2023-10-03 20:06:08 +0800

    chore: examine that multiple stream on the same deivce is possible

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ use rathernet::{
     rather::{AtherOutputStream, AtherStreamConfig},
     raudio::{AsioDevice, AudioInputStream, AudioOutputStream},
 };
-use std::fs;
+use std::fs::File;
+use std::io;
 use tokio_stream::StreamExt;
 
 #[tokio::main]
@@ -23,87 +24,58 @@ async fn main() -> Result<()> {
         default_config.sample_format(),
     );
 
+    let read_stream = AudioInputStream::try_from_device_config(&device, config.clone())?;
     let write_stream = AudioOutputStream::try_from_device_config(&device, config.clone())?;
-    let read_stream: AudioInputStream<f32> =
-        AudioInputStream::try_from_device_config(&device, config.clone())?;
 
-    let config = AtherStreamConfig::new(10000, 5000, config);
-    let write_ather = AtherOutputStream::new(config.clone(), write_stream);
+    let monitor_stream = AudioInputStream::try_from_device_config(&device, config.clone())?;
+
+    let config = AtherStreamConfig::new(10000, 15000, config.clone());
+    
     let mut read_ather = AtherInputStream::new(config.clone(), read_stream);
+    let write_ather = AtherOutputStream::new(config.clone(), write_stream);
 
-    // let file = fs::read_to_string("input.txt")?;
-    let file = fs::read_to_string("assets/ather/INPUT.txt")?;
+    let mut monitor_ather = AtherInputStream::new(config.clone(), monitor_stream);
+
     let mut bits = bitvec![];
-    for ch in file.chars() {
-        match ch {
-            '0' => bits.push(false),
-            '1' => bits.push(true),
-            _ => {}
+    let mut file = File::open("./assets/acsma/INPUT.bin")?;
+    io::copy(&mut file, &mut bits)?;
+
+    let write_future = async {
+        for chunk in bits.chunks(PAYLOAD_BITS_LEN) {
+            write_ather.write(chunk).await.unwrap();
         }
-    }
+    };
+    let read_future = async {
+        let mut buf = bitvec![0; bits.len()];
+        read(&mut read_ather, &mut buf).await;    
+        buf    
+    };
+    let monitor_future = async {
+        let mut buf = bitvec![0; bits.len()];
+        read(&mut monitor_ather, &mut buf).await;
+        buf
+    };
 
-    tokio::join!(
-        async {
-            for chunk in bits.chunks(PAYLOAD_BITS_LEN) {
-                write_ather.write(chunk).await.unwrap();
-            }
-            eprintln!("Transmitted: {}", bits.len());
-        },
-        async {
-            // let samples = read_stream.read_timeout(Duration::from_secs(5)).await;
-            // fs::write("samples.txt", format!("{:?}", samples)).unwrap();
+    let (_, mut read_buf, mut monitor_buf) = tokio::join!(write_future, read_future, monitor_future);
 
-            let mut buf = bitvec![];
-            while let Some(frame) = read_ather.next().await {
-                let len = frame.len();
-                buf.extend(frame);
-                eprintln!("Received: {}, new: {}", buf.len(), len);
-                if len != PAYLOAD_BITS_LEN {
-                    break;
-                }
-            }
-            eprintln!("Received: {}", buf.len());
-            fs::write(
-                "output.txt",
-                buf.into_iter()
-                    .map(|bit| if bit { '1' } else { '0' })
-                    .collect::<String>(),
-            )
-            .unwrap();
-        }
-    );
+    let file = File::create("output.bin")?;
+    io::copy(&mut read_buf, &mut &file)?;
 
-    // let mut buf = vec![];
-    // let binding = Decoder::new(BufReader::new(File::open("test.wav")?))?
-    //     .map(f32::from_sample_)
-    //     .collect::<Vec<f32>>();
-    // let mut stream = binding.chunks(384);
-    // let mut bits = bitvec![];
-    // let mut id = 0;
-    // while let Some(data) = rather::decode_from_buf(&config, &mut stream, &mut buf).await {
-    //     let len = data.len();
-    //     eprintln!("[{}] Received: {}", id, len);
-    //     bits.extend(data);
-    //     id += 1;
-    //     if len != 127 {
-    //         break;
-    //     }
-    // }
-
-    // Create a new file and write the bits to it as a string
-    // let mut file = File::create("output.txt")?;
-    // let content = bits
-    //     .iter()
-    //     .map(|bit| if *bit { '1' } else { '0' })
-    //     .collect::<String>();
-    // file.write_all(content.as_bytes())?;
-
-    // let mut buf = Decoder::new(BufReader::new(File::open("rpal.wav")?))?
-    //     .map(f32::from_sample_)
-    //     .collect::<Vec<f32>>();
-    // // buf.band_pass(48000., (9000., 11000.));
-    // // let track = AudioTrack::new(config.stream_config, buf.into());
-    // fs::write("rpal.json", format!("{:?}", buf)).unwrap();
+    let file = File::create("monitor.bin")?;
+    io::copy(&mut monitor_buf, &mut &file)?;
 
     Ok(())
+}
+
+async fn read(ather: &mut AtherInputStream, buf: &mut BitSlice) {
+    let mut bits = bitvec![];
+    while let Some(frame) = ather.next().await {
+        let len = frame.len();
+        bits.extend(frame);
+        eprintln!("Received: {}, new: {}", bits.len(), len);
+        if bits.len() >= buf.len() {
+            break;
+        }
+    }
+    buf.copy_from_bitslice(&bits);
 }
