0425504fe1937816c1d84378fbf1016b8e72f0ea
winlere <winlere@qq.com>
2023-10-07 01:58:07 +0800

    wperf: fine tune parameters for better throughput

--- src/racsma/builtin.rs
+++ src/racsma/builtin.rs
@@ -31,11 +31,11 @@ pub const PAYLOAD_BITS_LEN: usize = ATHER_PAYLOAD_BITS_LEN
     - PARITY_BITS_LEN;
 
 pub const SOCKET_SLOT_TIMEOUT: Duration = Duration::from_millis(200);
-pub const SOCKET_ACK_TIMEOUT: Duration = Duration::from_millis(100);
-pub const SOCKET_RECIEVE_TIMEOUT: Duration = Duration::from_millis(100);
+pub const SOCKET_ACK_TIMEOUT: Duration = Duration::from_millis(200);
+pub const SOCKET_RECIEVE_TIMEOUT: Duration = Duration::from_millis(50);
 
 pub const SOCKET_MAX_RESENDS: usize = 50;
-pub const SOCKET_MAX_BACKOFF: usize = 16;
+pub const SOCKET_MAX_BACKOFF: usize = 3; // 2 | 3 | 4 | 5 | 8 | 16
 
 pub const SOCKET_FREE_THRESHOLD: f32 = 0.001;
 pub const SOCKET_COLLISSION_THRESHOLD: f32 = 0.5;
