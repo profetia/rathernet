f07a5c0e57719e6644155ceb796bf7d65edb3d4a
winlere <winlere@qq.com>
2023-10-10 22:10:20 +0800

    perf: 20s semi duplex

--- src/racsma/builtin.rs
+++ src/racsma/builtin.rs
@@ -30,12 +30,12 @@ pub const PAYLOAD_BITS_LEN: usize = ATHER_PAYLOAD_BITS_LEN
     - SEQ_BITS_LEN
     - PARITY_BITS_LEN;
 
-pub const SOCKET_SLOT_TIMEOUT: Duration = Duration::from_millis(100);
-pub const SOCKET_ACK_TIMEOUT: Duration = Duration::from_millis(200);
-pub const SOCKET_RECIEVE_TIMEOUT: Duration = Duration::from_millis(50);
+pub const SOCKET_SLOT_TIMEOUT: Duration = Duration::from_millis(85);
+pub const SOCKET_ACK_TIMEOUT: Duration = Duration::from_millis(30);
+pub const SOCKET_RECIEVE_TIMEOUT: Duration = Duration::from_millis(25);
 
 pub const SOCKET_MAX_RESENDS: usize = 30;
-pub const SOCKET_MAX_RANGE: usize = 6; 
+pub const SOCKET_MAX_RANGE: usize = 6;
 
 pub const SOCKET_FREE_THRESHOLD: f32 = 1e-4;
 pub const SOCKET_COLLISSION_THRESHOLD: f32 = 0.5;
