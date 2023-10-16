146882c869abe59076407d22c0e70aada8a1567f
yanglinshu <nicolasvandarkholme@outlook.com>
2023-10-16 21:51:35 +0800

    chore: add script for part 3

---
+++ scripts/rateway/part3/ch1.ps1
@@ -0,0 +1,19 @@
+git checkout -b part3 a3353f1
+
+cargo build --release
+
+# Start node 2 in administrator mode as the NAT server
+./target/release/rateway nat -c ./assets/ateway/node2.toml
+
+# Start node 1 as the sender
+./target/release/rateway install -c ./assets/ateway/node1.toml
+
+# Wait for the installation to complete
+# Set up the route table
+
+# Ping node 3
+ping "ipv4" -S "ipv4" -t
+
+git checkout main
+
+git branch -D part3
