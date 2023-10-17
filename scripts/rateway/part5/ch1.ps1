1f3111a7d75ebdabf7d5bf1796a7ba868990af19
winlere <winlere@qq.com>
2023-10-17 20:06:39 +0800

    chore: add script for part 5

---
+++ scripts/rateway/part5/ch1.ps1
@@ -0,0 +1,5 @@
+# Same as part 4. Others are required to implement the same mechanism as rathernet to achieve the 
+# same result. In rathernet, the gateway uses the identification field of the ICMP echo to 
+# construct mapping between the internal and external IP addresses. The gateway then uses the
+# mapping to translate the IP addresses in the ICMP echo reply. Other gateways should have exactly
+# the same behavior.
