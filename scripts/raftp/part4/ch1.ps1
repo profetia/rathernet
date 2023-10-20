8ff710cfcdbaa2fb320f51ff2fd377fffce7ffc1
yanglinshu <nicolasvandarkholme@outlook.com>
2023-10-20 19:10:24 +0800

    chore: add script for part 4

---
+++ scripts/raftp/part4/ch1.ps1
@@ -0,0 +1,4 @@
+# Do nothing since our implementation is tunnel-based in the first place.
+
+# Install the adapter and start the nat, and you are good to go.
+# Either using FileZilla for FTP or use Chrome for HTTP is fine.
