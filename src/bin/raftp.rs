bd77788546f3aafdcd341cff0b179335b9361c47
yanglinshu <nicolasvandarkholme@outlook.com>
2023-10-20 18:31:16 +0800

    feat: ls on raftp

--- src/bin/raftp.rs
+++ src/bin/raftp.rs
@@ -1,7 +1,6 @@
 use anyhow::Result;
 use clap::Parser;
-use rathernet::raftp::AftpEditor;
-use thiserror::Error;
+use rathernet::raftp::{AftpEditor, AftpIoError};
 
 #[derive(Parser, Debug)]
 #[clap(name = "raftp", version = "0.1.0")]
@@ -12,34 +11,27 @@ struct RaftpCli {
     url: String,
 }
 
-fn parse_url(url: &str) -> Result<(String, String)> {
+fn parse_url(url: &str) -> Result<(String, String, u16)> {
     let mut split = url.splitn(2, '@');
     let Some(user) = split.next() else {
-        return Err(RaftpError::InvalidUrl(url.into()).into());
+        return Err(AftpIoError::InvalidUrl(url.into()).into());
     };
     let Some(rest) = split.next() else {
-        return Err(RaftpError::InvalidUrl(url.into()).into());
+        return Err(AftpIoError::InvalidUrl(url.into()).into());
     };
     let mut split = rest.splitn(2, ':');
     let Some(host) = split.next() else {
-        return Err(RaftpError::InvalidUrl(url.into()).into());
+        return Err(AftpIoError::InvalidUrl(url.into()).into());
     };
     let port = split.next().map(|s| s.parse::<u16>()).unwrap_or(Ok(21))?; // Default to port 21
-    Ok((user.into(), format!("{}:{}", host, port)))
+    Ok((user.into(), host.into(), port))
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let cli = RaftpCli::parse();
-    let (user, host) = parse_url(&cli.url)?;
-    let mut editor = AftpEditor::try_new(&user, &host).await?;
-    editor.login().await?;
-
+    let (user, host, port) = parse_url(&cli.url)?;
+    let mut editor = AftpEditor::try_new(&user, &host, port).await?;
+    editor.repl().await?;
     Ok(())
-}
-
-#[derive(Debug, Error)]
-enum RaftpError {
-    #[error("Invalid URL: {0}")]
-    InvalidUrl(String),
 }
--- src/raftp/mod.rs
+++ src/raftp/mod.rs
@@ -4,3 +4,17 @@ pub use editor::AftpEditor;
 
 pub use async_ftp::FtpError;
 pub use async_ftp::FtpStream;
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum AftpIoError {
+    #[error("Invalid URL: {0}")]
+    InvalidUrl(String),
+    #[error("Invalid command: {0}")]
+    InvalidCommand(String),
+    #[error("Invalid arguments")]
+    InvalidArguments,
+    #[error("{0}")]
+    FtpError(#[from] FtpError),
+}
