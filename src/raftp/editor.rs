9d9da8d47db4e9a3b3379d1114ca6751250e69ec
yanglinshu <nicolasvandarkholme@outlook.com>
2023-10-20 18:31:16 +0800

    feat: ls on raftp

--- src/raftp/editor.rs
+++ src/raftp/editor.rs
@@ -2,33 +2,79 @@ use anyhow::Result;
 use async_ftp::FtpStream;
 use rustyline::{history::FileHistory, Completer, Editor, Helper, Highlighter, Hinter, Validator};
 
+use super::AftpIoError;
+
 pub struct AftpEditor {
     user: String,
     host: String,
+    workspace: String,
     stream: FtpStream,
     editor: Editor<AftpEditorHelper, FileHistory>,
 }
 
 impl AftpEditor {
-    pub async fn try_new(user: &str, host: &str) -> Result<Self> {
+    pub async fn try_new(user: &str, host: &str, port: u16) -> Result<Self> {
         let mut editor = Editor::new()?;
         editor.set_helper(Some(AftpEditorHelper {}));
-        let stream = FtpStream::connect(host).await?;
+        let url = format!("{}:{}", host, port);
+        let mut stream = FtpStream::connect(url).await?;
+
+        let prompt = format!("{}@{}'s password: ", user, host);
+        let password = editor.readline(&prompt)?;
+        stream.login(user, &password).await?;
+        if let Some(welcome) = stream.get_welcome_msg() {
+            println!("{}", welcome);
+        }
+        let workspace = stream.pwd().await?;
 
         Ok(Self {
             user: user.into(),
             host: host.into(),
+            workspace,
             stream,
             editor,
         })
     }
 
-    pub async fn login(&mut self) -> Result<()> {
-        let prompt = format!("{}@{}'s password: ", self.user, self.host);
-        let password = self.editor.readline(&prompt)?;
-        self.stream.login(&self.user, &password).await?;
-        Ok(())
+    pub async fn repl(&mut self) -> Result<()> {
+        loop {
+            let line = self
+                .editor
+                .readline(&format!("{}@{} {}# ", self.user, self.host, self.workspace))?;
+            match self.execute(&line).await {
+                Ok(_) => {}
+                Err(AftpIoError::FtpError(err)) => return Err(err.into()),
+                Err(err) => println!("{}", err),
+            };
+        }
     }
+}
+
+impl AftpEditor {
+    async fn execute(&mut self, line: &str) -> Result<(), AftpIoError> {
+        let mut args = line.split_whitespace();
+        let Some(command) = args.next() else {
+            return Err(AftpIoError::InvalidCommand(line.into()));
+        };
+
+        match command {
+            "ls" => command_ls(self, args.collect::<Vec<_>>().as_slice()).await,
+            _ => Err(AftpIoError::InvalidCommand(command.into())),
+        }
+    }
+}
+
+async fn command_ls(editor: &mut AftpEditor, args: &[&str]) -> Result<(), AftpIoError> {
+    let result = editor.stream.list(args.first().copied()).await;
+    match result {
+        Ok(list) => {
+            for item in list {
+                println!("{}", item);
+            }
+        }
+        Err(err) => return Err(AftpIoError::FtpError(err)),
+    }
+    Ok(())
 }
 
 #[derive(Completer, Helper, Hinter, Validator, Highlighter)]
