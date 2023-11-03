e852eab9084af8db6adaa876434f180e2100fccc
winlere <winlere@qq.com>
2023-11-03 15:09:44 +0800

    revert: pack up for merge into main

--- src/rather/frame.rs
+++ src/rather/frame.rs
@@ -1,4 +1,3 @@
-use super::stream::{SWEEP_ENDBAND, SWEEP_GAP, SWEEP_STARTBAND};
 use crate::raudio::AudioSamples;
 use std::f32::consts::PI;
 
@@ -55,29 +54,16 @@ impl From<Preamble> for AudioSamples<f32> {
 pub struct Symbol(pub AudioSamples<f32>);
 
 impl Symbol {
-    pub fn new(_frequency: u32, sample_rate: u32, duration: f32) -> (Self, Self) {
-        let len = (duration as f32 * sample_rate as f32) as i32;
-        let zero_sweep = (0..len)
+    pub fn new(frequency: u32, sample_rate: u32, duration: f32) -> (Self, Self) {
+        let zero = (0..(duration * sample_rate as f32) as usize)
             .map(|item| {
-                SWEEP_STARTBAND + item as f32 * (SWEEP_ENDBAND - SWEEP_STARTBAND) / len as f32
+                let t = item as f32 * 2.0 * PI / sample_rate as f32;
+                (t * frequency as f32).sin()
             })
-            .scan(0.0f32, |acc, item| {
-                *acc += item / sample_rate as f32;
-                Some(*acc)
-            })
-            .map(|item| (item * 2.0 * PI).sin())
             .collect::<AudioSamples<f32>>();
-        let one_sweep = (0..len)
-            .map(|item| {
-                SWEEP_STARTBAND + SWEEP_GAP + item as f32 * (SWEEP_ENDBAND - SWEEP_STARTBAND) / len as f32
-            })
-            .scan(0.0f32, |acc, item| {
-                *acc += item / sample_rate as f32;
-                Some(*acc)
-            })
-            .map(|item| (item * 2.0 * PI).sin())
-            .collect::<AudioSamples<f32>>();
-        (Self(zero_sweep), Self(one_sweep))
+        let one = zero.iter().map(|item| -item).collect::<AudioSamples<f32>>();
+
+        (Self(zero), Self(one))
     }
 }
 
